1316b859d2ece51c83ce86eef50a9b57e551f111
Marcos <marcospastora@gmail.com>
2023-01-09 14:46:14 -0300

    adicionando swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Quiz App backend API",
+ *      description="Documentação da API desenvolvida para o quiz app",
+ *      @OA\Contact(
+ *          email="marcospastora@gmail.com"
+ *      )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
